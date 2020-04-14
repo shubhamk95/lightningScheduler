3f2517b59033def18cb6d7c48db19195148fcf1f
shubham <imshubham1410@gmail.com>
2020-04-14 16:56:28 +0530

    modified

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', {page:'Home', menuId:'home'});
 });
 
-router.get('/about', function(req, res, next) {
-  res.render('about', {page:'About Us', menuId:'about'});
+router.get('/cancelAppointment', function(req, res, next) {
+  res.render('cancelAppointment', {page:'Cancel Appointment', menuId:''});
 });
 
-router.get('/contact', function(req, res, next) {
-  res.render('contact', {page:'Contact Us', menuId:'contact'});
+router.get('/modifyAppointment', function(req, res, next) {
+  res.render('modifyAppointment', {page:'Modify Appointment', menuId:''});
 });
 
 module.exports = router;
